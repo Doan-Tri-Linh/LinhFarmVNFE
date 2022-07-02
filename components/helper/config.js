73474c5fd5041332b5a185c8ab918d25fe656188
Doan Tri Linh <5951071050@st.utc2.edu.vn>
2022-07-03 02:49:20 +0700

    test ngrok

--- components/helper/config.js
+++ components/helper/config.js
@@ -1,2 +1,3 @@
-export const BASEURL = "http://127.0.0.1:5000";
-// export const BASEURL = "https://linhfarm.herokuapp.com";
+// export const BASEURL = "http://127.0.0.1:5000";
+// export const BASEURL = "https://4b93-2405-4803-c845-be90-f874-ad60-b4b9-b96.ap.ngrok.io";
+export const BASEURL = "https://linhfarm.herokuapp.com";
